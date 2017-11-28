e703811fcbec0aadd5bbea7afcfc50ee4117fbd8
StanSiska <stanislav.siska@gmail.com>
2017-11-28 17:29:35 +0100

    Adding shell scripts for Jenkins master deployment

---
+++ 03-AWS-Builder-AMI/scripts/jenkins-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Wait 10 seconds before we begin with SSH connection ..."
+sleep 10
+
+echo "Adding Jenkins repository ..."
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+
+echo "Installing Jenkins ..."
+sudo yum install jenkins -y 
+sudo service jenkins start
+
+echo "Jenkins configured to start after boot"
+sudo chkconfig on jenkins
